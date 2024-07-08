dd5eb53da9f906e508ca3e1843a12c57830b4474
andres restrepo <arestrepo@vozy.co>
2024-07-08 14:31:55 +0200

    Adding main function exporting torchscript

--- src/export.py
+++ src/export.py
@@ -39,3 +39,25 @@ def parse_opt(known=False):
     parser.add_argument("--imgsz", "--img", "--img-size", type=int, default=224, help="train, val image size (pixels)")
 
     return parser.parse_known_args()[0] if known else parser.parse_args()
+
+
+def main(parameters):
+    export(parameters)
+
+
+def run(**kwargs):
+    """
+    Executes YOLOv5 training with given options, overriding with any kwargs provided.
+
+    Example: import train; train.run(data='coco128.yaml', imgsz=320, weights='yolov5m.pt')
+    """
+    opt = parse_opt(True)
+    for k, v in kwargs.items():
+        setattr(opt, k, v)
+    main(opt)
+    return opt
+
+
+if __name__ == "__main__":
+    opt = parse_opt()
+    main(opt)
